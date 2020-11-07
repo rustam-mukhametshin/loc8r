f5c54bb1603fd3521afaeac34e05f3cb5ac2c222
iproman <itechman@yandex.ru>
2020-11-07 09:19:40 +0500

    Separate route from controller

---
+++ app_server/controllers/main.js
@@ -0,0 +1,7 @@
+const index = function (req, res) {
+    res.render('index', {title: 'Express'});
+}
+
+module.exports = {
+    index
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {index} = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', index);
 
 module.exports = router;
