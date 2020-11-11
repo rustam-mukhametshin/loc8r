35a2c50eec5bfa575c39363308739084cdb307fa
iproman <itechman@yandex.ru>
2020-11-11 13:19:32 +0500

    Add location controller

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,33 @@
+/**
+ * Get 'home' page
+ * @param req
+ * @param res
+ */
+const homelist = function (req, res) {
+    res.render('index', {title: 'Home'});
+}
+
+/**
+ * Get 'Location info' page
+ * @param req
+ * @param res
+ */
+const locationInfo = function (req, res) {
+    res.render('index', {title: 'Location info'});
+}
+
+/**
+ * Get 'Add review' page
+ * @param req
+ * @param res
+ */
+const addReview = function (req, res) {
+    res.render('index', {title: 'Add review'});
+}
+
+
+module.exports = {
+    homelist,
+    locationInfo,
+    addReview
+};
