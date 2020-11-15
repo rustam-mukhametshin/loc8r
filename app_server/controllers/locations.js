46e3361090e1d10d5cf7ab6d2f0a5844f60e2792
iproman <itechman@yandex.ru>
2020-11-15 22:22:02 +0500

    Divide data to controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -9,7 +9,26 @@ const homelist = function (req, res) {
         pageHeader: {
             title: 'Loc8r',
             strapline: 'Find places to work with wifi near you!'
-        }
+        },
+        locations: [{
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '100m'
+        }, {
+            name: 'Cafe Hero',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 4,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '200m'
+        }, {
+            name: 'Burger Queen',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 2,
+            facilities: ['Food', 'Premium wifi'],
+            distance: '250m'
+        }]
     });
 }
 
