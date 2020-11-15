20f4f01ebc2bad1b3d1a74272fc1bd1e24677015
iproman <itechman@yandex.ru>
2020-11-16 00:31:17 +0500

    About data

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -4,7 +4,10 @@
  * @param res
  */
 const about = function (req, res) {
-    res.render('generic-text', {title: 'About'});
+    res.render('generic-text', {
+        title: 'About',
+        content: 'Loc8r was created to help people find places to sit down and get a bit of work done. \n Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nuncsed lorem ac nisi dignissim accumsan.'
+    });
 }
 
 module.exports = {
