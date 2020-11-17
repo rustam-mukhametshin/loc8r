f22d47d06c224d1c4c3a0762ea8a29085c15371b
iproman <itechman@yandex.ru>
2020-11-17 13:12:19 +0500

    Emit the SIGINT event for windows

---
+++ app_server/models/db.js
@@ -0,0 +1,12 @@
+const readLine = require('readline');
+
+// Emit the SIGINT event for windows
+if (process.platform === 'win32') {
+    const rl = readLine.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.on('SIGINT', () => {
+        process.emit("SIGINT");
+    })
+}
