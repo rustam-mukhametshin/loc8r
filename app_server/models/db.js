58105a36bff002e070990558564308658ec0368c
iproman <itechman@yandex.ru>
2020-11-17 13:13:09 +0500

    Listeners for Mongoose connection

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -15,3 +15,16 @@ if (process.platform === 'win32') {
 // Define DB and open Mongoose connection
 const dbURI = 'mongodb://localhost/Loc8r';
 mongoose.connect(dbURI, {useNewUrlParser: true});
+
+// Listeners for Mongoose connection
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${dbURI}`);
+})
+
+mongoose.connection.on('error', err => {
+    console.log('Mongoose connected error: ', err);
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+})
