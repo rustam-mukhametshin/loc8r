77fa1848ed6cbeccba6349f15d2a707fc6eca9dd
iproman <itechman@yandex.ru>
2020-11-18 19:23:21 +0500

    Add location schema

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -57,3 +57,5 @@ process.on('SIGTERM', () => {
         process.exit(0);
     });
 })
+
+require('./locations');
---
+++ app_server/models/locations.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const {Schema} = mongoose.Schema;
+
+const locationSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: String,
+    rating: {
+        type: Number,
+        'default': 0,
+        min: 0,
+        max: 5
+    },
+    facilities: [String]
+})
