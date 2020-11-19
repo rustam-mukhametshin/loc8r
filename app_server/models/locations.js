06027056bc23a6a777dc1cc1c6e8c6f6a3a4b815
iproman <itechman@yandex.ru>
2020-11-19 13:21:50 +0500

    Add openingTimeSchema

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -2,6 +2,19 @@ const mongoose = require("mongoose");
 
 const {Schema} = mongoose;
 
+const openingTimeSchema = new Schema({
+    days: {
+        type: String,
+        required: true
+    },
+    opening: String,
+    closing: String,
+    closed: {
+        type: Boolean,
+        required: true
+    }
+})
+
 const locationSchema = new Schema({
     name: {
         type: String,
@@ -18,7 +31,8 @@ const locationSchema = new Schema({
     coords: {
         type: {type: String},
         coordinates: [Number]
-    }
+    },
+    openingTimes: [openingTimeSchema]
 });
 
 locationSchema.index({coords: '2dsphere'})
