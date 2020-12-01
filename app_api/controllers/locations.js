5cfbc71578d35f51d99419a7a18252dc7d70f50c
iproman <itechman@yandex.ru>
2020-12-02 02:10:08 +0500

    Add empty methods

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,51 @@
+/**
+ *
+ * @param req
+ * @param res
+ */
+const locationsDeleteOne = (req, res) => {
+
+};
+
+/**
+ *
+ * @param req
+ * @param res
+ */
+const locationsUpdateOne = (req, res) => {
+
+};
+
+/**
+ *
+ * @param req
+ * @param res
+ */
+const locationsReadOne = (req, res) => {
+
+};
+
+/**
+ *
+ * @param req
+ * @param res
+ */
+const locationsCreate = (req, res) => {
+
+};
+/**
+ *
+ * @param req
+ * @param res
+ */
+const locationsListByDistance = (req, res) => {
+
+};
+
+module.exports = {
+    locationsDeleteOne,
+    locationsUpdateOne,
+    locationsReadOne,
+    locationsCreate,
+    locationsListByDistance,
+};
---
+++ app_api/controllers/reviews.js
@@ -0,0 +1,43 @@
+/**
+ *
+ * @param req
+ * @param res
+ */
+const reviewsDeleteOne = (req, res) => {
+
+};
+
+/**
+ *
+ * @param req
+ * @param res
+ */
+const reviewsUpdateOne = (req, res) => {
+
+};
+
+/**
+ *
+ * @param req
+ * @param res
+ */
+const reviewsReadOne = (req, res) => {
+
+};
+
+/**
+ *
+ * @param req
+ * @param res
+ */
+const reviewsCreate = (req, res) => {
+
+};
+
+
+module.exports = {
+    reviewsDeleteOne,
+    reviewsUpdateOne,
+    reviewsReadOne,
+    reviewsCreate
+}
