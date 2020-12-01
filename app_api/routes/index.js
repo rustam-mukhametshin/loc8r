1cc6462ecfe7f644483d2bbb2c7ccdb2cf4bdf19
iproman <itechman@yandex.ru>
2020-12-02 02:07:06 +0500

    Define high level controller actions

---
+++ app_api/routes/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const ctrlLocations = require('../controllers/locations');
+const ctrlReviews = require('../controllers/reviews');
+
+// Locations
+router
+    .route('/locations')
+    .get(ctrlLocations.locationsListByDistance)
+    .post(ctrlLocations.locationsCreate);
+
+router
+    .route('/locations/:locationId')
+    .get(ctrlLocations.locationsReadOne)
+    .put(ctrlLocations.locationsUpdateOne)
+    .delete(ctrlLocations.locationsDeleteOne);
+
+// Reviews
+router
+    .route('/locations/:locationId/reviews')
+    .post(ctrlReviews.reviewsCreate)
+
+router
+    .route('/locations/:locationId/reviews/:reviewId')
+    .get(ctrlReviews.reviewsReadOne)
+    .put(ctrlReviews.reviewsUpdateOne)
+    .delete(ctrlReviews.reviewsDeleteOne);
+
+module.exports = router;
