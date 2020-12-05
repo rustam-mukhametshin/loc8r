812a434864716dc3d2d50e6225e6007a7c6718b0
iproman <itechman@yandex.ru>
2020-12-05 05:27:44 +0500

    Api method for showing one review of spec location

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,3 +1,7 @@
+const mongoose = require('mongoose');
+
+const Loc = mongoose.model('Location');
+
 /**
  *
  * @param req
@@ -17,12 +21,57 @@ const reviewsUpdateOne = (req, res) => {
 };
 
 /**
- *
+ * Show one review.
  * @param req
  * @param res
  */
 const reviewsReadOne = (req, res) => {
+    Loc
+        .findById(req.params.locationId)
+        .select('name reviews')
+        .exec((err, loc) => {
 
+            if (!loc) {
+                return res
+                    .status(404)
+                    .json({
+                        message: 'Location not found'
+                    })
+
+            } else if (err) {
+                return res
+                    .status(404)
+                    .json(err)
+            }
+
+            if (loc.reviews && loc.reviews.length > 0) {
+                const review = loc.reviews.id(req.params.reviewId);
+
+                if (!review) {
+                    return res
+                        .status(400)
+                        .json({message: 'Review not found'})
+                } else {
+                    let response = {
+                        location: {
+                            name: loc.name,
+                            id: req.params.locationId
+                        },
+                        review
+                    };
+                    return res
+                        .status(200)
+                        .json(response)
+                }
+            } else {
+                res
+                    .status(404)
+                    .json({
+                        message: 'No reviews found'
+                    });
+            }
+
+        });
 };
 
 /**
