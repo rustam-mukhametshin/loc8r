9258fde798d2586cd221e534b05b2f03bd23b6b8
iproman <itechman@yandex.ru>
2020-12-24 01:56:48 +0500

    Render in separate method

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -4,34 +4,7 @@
  * @param res
  */
 const homelist = function (req, res) {
-    res.render('locations-list', {
-        title: 'Loc8r - find a palce to work with wifi',
-        description: 'Looking for wifi and a seat? Loc8r helps you find places to work when out and about.',
-        pageHeader: {
-            title: 'Loc8r',
-            strapline: 'Find places to work with wifi near you!'
-        },
-        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-        locations: [{
-            name: 'Starcups',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 3,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '100m'
-        }, {
-            name: 'Cafe Hero',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 4,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '200m'
-        }, {
-            name: 'Burger Queen',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 2,
-            facilities: ['Food', 'Premium wifi'],
-            distance: '250m'
-        }]
-    });
+    renderHomepage(req, res);
 }
 
 /**
@@ -100,6 +73,43 @@ const addReview = function (req, res) {
     res.render('location-review-form', {
         title: 'Add review',
         pageHeader: {title: 'Review Starcups'}
+    });
+}
+
+/**
+ * Render homepage
+ *
+ * @param req
+ * @param res
+ */
+const renderHomepage = (req, res) => {
+    res.render('locations-list', {
+        title: 'Loc8r - find a palce to work with wifi',
+        description: 'Looking for wifi and a seat? Loc8r helps you find places to work when out and about.',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near you!'
+        },
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+        locations: [{
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '100m'
+        }, {
+            name: 'Cafe Hero',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 4,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '200m'
+        }, {
+            name: 'Burger Queen',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 2,
+            facilities: ['Food', 'Premium wifi'],
+            distance: '250m'
+        }]
     });
 }
 
