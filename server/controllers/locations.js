151a6363ea977ac82e773876af1663ca78707901
iproman <itechman@yandex.ru>
2020-12-30 07:15:34 +0500

    Method for adding reviews

--- server/controllers/locations.js
+++ server/controllers/locations.js
@@ -200,8 +200,39 @@ const getLocationInfo = (req, res, callback) => {
 
 /**
  * Add review
+ *
+ * @param req
+ * @param res
  */
 const doAddReviewAction = (req, res) => {
+    const locationId = req.params.locationId;
+
+    if (locationId) {
+
+        const path = `/api/locations/${locationId}/reviews`;
+
+        let body = req.body;
+
+        const postData = {
+            author: body.name,
+            rating: parseInt(body.rating, 10),
+            reviewText: body.review
+        };
+
+        const requestOptions = {
+            url: `${apiOptions.server}${path}`,
+            method: 'POST',
+            json: postData
+        }
+
+        request(requestOptions, (err, {statusCode}, body) => {
+            if (statusCode === 201) {
+                res.redirect(`/location/${locationId}`);
+            } else {
+                showError(req, res, statusCode);
+            }
+        })
+    }
 
 }
 
