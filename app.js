434e159783fe679120d8e516c675278b1b7c2f61
iproman <itechman@yandex.ru>
2021-01-07 03:43:10 +0500

    Fix CORS issue in backend api for angular app

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
