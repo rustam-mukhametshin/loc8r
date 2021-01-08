dd9aa4df61105907db83ffd1557740e2b8158ab2
iproman <itechman@yandex.ru>
2021-01-09 03:22:58 +0500

    Angular folder as public for express

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_public'))); // Angular
 
 app.use('/api', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
