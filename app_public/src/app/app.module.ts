1add3117603157670baf9c45f97e32c9348fd5c0
iproman <itechman@yandex.ru>
2021-01-09 04:13:46 +0500

    Add about component

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -5,19 +5,22 @@ import { DistancePipe } from './pipes/distance.pipe';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { FrameworkComponent } from './views/framework/framework.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
     HomeListComponent,
     DistancePipe,
-    FrameworkComponent
+    FrameworkComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot([
       {path: '', component: HomeListComponent},
+      {path: '', component: AboutComponent},
     ])
   ],
   providers: [],
