904def954d38fe814bb002b9350a53fe31a19a14
iproman <itechman@yandex.ru>
2021-01-11 10:02:47 +0500

    Change to dynamic data

--- app_public/src/app/views/sidebar/sidebar.component.ts
+++ app_public/src/app/views/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  @Input() content: string;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
