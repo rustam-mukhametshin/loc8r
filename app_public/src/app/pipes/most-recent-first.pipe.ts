35977f795521bb2f5038662a2f79f6852c90ca38
iproman <itechman@yandex.ru>
2021-01-13 00:42:06 +0500

    Order reviews by date

---
+++ app_public/src/app/pipes/most-recent-first.pipe.ts
@@ -0,0 +1,29 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Review } from '../models/Location';
+
+@Pipe({
+  name: 'mostRecentFirst'
+})
+export class MostRecentFirstPipe implements PipeTransform {
+
+  private compare(a, b) {
+    const createdOnA = a.createdOn;
+    const createdOnB = b.createdOn;
+
+    let comparison = 1;
+
+    if (createdOnA > createdOnB) {
+      comparison = -1;
+    }
+
+    return comparison;
+  }
+
+  transform(reviews: Review[]): Review[] | null {
+    if (reviews && reviews.length > 0) {
+      return reviews.sort(this.compare);
+    }
+    return null;
+  }
+
+}
