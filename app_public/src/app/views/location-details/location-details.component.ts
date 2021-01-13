28fc6fa1aadba8a64dc178fcce5e31fd9bab6967
iproman <itechman@yandex.ru>
2021-01-13 17:16:10 +0500

    Add form for reviews and bind data

--- app_public/src/app/views/location-details/location-details.component.ts
+++ app_public/src/app/views/location-details/location-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Location } from '../../models/Location';
+import { Review } from '../../models/Review';
 
 @Component({
   selector: 'app-location-details',
@@ -11,6 +12,12 @@ export class LocationDetailsComponent implements OnInit {
   @Input() location: Location;
 
   public googleAPIKey = 'Put your Google Maps API Key here';
+
+  public newReview: Review = {
+    author: '',
+    rating: 5,
+    reviewText: ''
+  };
 
   constructor() {
   }
