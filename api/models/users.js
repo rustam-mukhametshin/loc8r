4c7e329446d666013a6e83f16f5cc0f21b47aa25
iproman <itechman@yandex.ru>
2021-01-15 01:59:56 +0500

    Add method to validate password

--- api/models/users.js
+++ api/models/users.js
@@ -25,4 +25,16 @@ userSchema.methods.setPassword = function (password) {
     this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
 }
 
+/**
+ * Validate password
+ *
+ * @param password
+ */
+userSchema.methods.validPassword = function (password) {
+    const hash = crypto
+        .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+
+    return this.hash === hash;
+}
+
 mongoose.model('User', userSchema);
