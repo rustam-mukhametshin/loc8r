41d6bd0971a63e515cd3f2e727678a32a2b89959
iproman <itechman@yandex.ru>
2021-01-19 22:34:24 +0500

    Authorization acceptable header

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(passport.initialize());
 
 app.use('/api', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 });
 
