42600140ace17182401d1a24ed881debe9467600
iproman <itechman@yandex.ru>
2021-01-20 01:45:03 +0500

    Add method to get author

--- api/controllers/reviews.js
+++ api/controllers/reviews.js
@@ -1,6 +1,40 @@
 const mongoose = require('mongoose');
 
 const Loc = mongoose.model('Location');
+const User = mongoose.model('User');
+
+/**
+ * Get author
+ *
+ * @param req
+ * @param res
+ * @param callback
+ */
+const getAuthor = (req, res, callback) => {
+    if (req.payload && req.payload.email) {
+        User
+            .findOne({email: req.payload.email})
+            .exec((err, user) => {
+
+                if (!user) {
+                    return res
+                        .status(404)
+                        .json({message: 'User not found'});
+                } else if (err) {
+                    console.log(err);
+                    return res
+                        .status(404)
+                        .json(err);
+                }
+
+                callback(req, res, user.name);
+            });
+    } else {
+        return res
+            .status(404)
+            .json({'message': 'User not found'});
+    }
+}
 
 /**
  * Delete review
