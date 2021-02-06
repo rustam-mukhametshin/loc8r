cc1f07fb45a5c5328fb5836ed0f7f0bf2e279698
iproman <itechman@yandex.ru>
2021-02-06 22:16:12 +0500

    Add function for global error messages

--- app_public/src/app/services/message.service.ts
+++ app_public/src/app/services/message.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
 
-  constructor() { }
+  private subject = new BehaviorSubject<string[]>([]);
+
+  errors$: Observable<string[]> = this.subject.asObservable()
+    .pipe(
+      filter(msg => msg && msg.length > 0)
+    )
+  ;
+
+  constructor() {
+  }
+
+  /**
+   * Show errors
+   */
+  showErrors(...errors: string[]) {
+    this.subject.next(errors);
+  }
 }
--- app_public/src/app/views/components/message/message.component.ts
+++ app_public/src/app/views/components/message/message.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from '../../../services/message.service';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-message',
@@ -8,11 +10,27 @@ import { MessageService } from '../../../services/message.service';
 })
 export class MessageComponent implements OnInit {
 
+  showMessages = false;
+
+  errors$: Observable<string[]>;
+
   constructor(
     private messageService: MessageService
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
+    this.errors$ = this.messageService.errors$
+      .pipe(
+        tap(() => this.showMessages = true)
+      );
+  }
+
+  /**
+   * Hide messages
+   */
+  onClose() {
+    this.showMessages = false;
   }
 
 }
