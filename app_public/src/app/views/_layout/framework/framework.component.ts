4c2a84fdc7a8a949636eab8c1588831631b67d5b
iproman <itechman@yandex.ru>
2021-03-13 15:14:15 +0500

    Refactor framework c to header

--- app_public/src/app/views/_layout/framework/framework.component.ts
+++ app_public/src/app/views/_layout/framework/framework.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from '../../services/authentication.service';
-import { User } from '../../classes/user';
 
 @Component({
   selector: 'app-framework',
@@ -10,33 +8,9 @@ import { User } from '../../classes/user';
 export class FrameworkComponent implements OnInit {
 
   constructor(
-    private authenticationService: AuthenticationService
   ) {
   }
 
   ngOnInit(): void {
-  }
-
-  /**
-   * Logout
-   */
-  public doLogout(): void {
-    this.authenticationService.logout();
-  }
-
-  /**
-   * Check if user is logged in
-   */
-  public isLoggedIn(): boolean {
-    return this.authenticationService.isLoggedIn();
-  }
-
-  /**
-   * Get username
-   */
-  public getUsername(): string {
-    const user: User = this.authenticationService.getCurrentUser();
-
-    return user ? user.name : 'Guest';
   }
 }
--- app_public/src/app/views/_layout/header/header.component.ts
+++ app_public/src/app/views/_layout/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../../services/authentication.service';
+import { User } from '../../../classes/user';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authenticationService: AuthenticationService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * Logout
+   */
+  public doLogout(): void {
+    this.authenticationService.logout();
+  }
+
+  /**
+   * Check if user is logged in
+   */
+  public isLoggedIn(): boolean {
+    return this.authenticationService.isLoggedIn();
+  }
+
+  /**
+   * Get username
+   */
+  public getUsername(): string {
+    const user: User = this.authenticationService.getCurrentUser();
+
+    return user ? user.name : 'Guest';
   }
 
 }
