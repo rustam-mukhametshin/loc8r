d6b285d0908b5c786e51c29b2c34d3849a7170c2
iproman <itechman@yandex.ru>
2021-03-16 17:29:14 +0500

    Fix storage tests

--- app_public/src/app/services/storage.service.spec.ts
+++ app_public/src/app/services/storage.service.spec.ts
@@ -5,25 +5,45 @@ import { StorageService } from './storage.service';
 describe('StorageService', () => {
   let serviceSpy: jasmine.SpyObj<StorageService>;
 
-  beforeEach(() => {
-    const spy = jasmine.createSpyObj('StorageService', [
-      'getItem',
-    ]);
+  // custom
+  let key: string;
+  let value: string;
 
+  beforeEach(() => {
+    serviceSpy = jasmine.createSpyObj('StorageService', [
+      'getItem',
+      'setItem',
+      'removeItem',
+    ]);
 
     TestBed.configureTestingModule({
       providers: [
         {
           provide: StorageService,
-          useValue: spy,
+          useValue: serviceSpy,
         }
       ]
     });
 
+    // custom
+    key = 'test-loc8r-token';
+    value = 'test-loc8r-value';
+  });
+
+  beforeEach(() => {
     serviceSpy = TestBed.inject(StorageService) as jasmine.SpyObj<StorageService>;
 
-    serviceSpy.getItem.and.returnValue('loc8r-value');
+    serviceSpy.getItem.and.callFake((k) => {
+      return localStorage.getItem(k);
+    });
+    serviceSpy.setItem.and.callFake((k, v) => {
+      localStorage.setItem(k, v);
+    });
+    serviceSpy.removeItem.and.callFake((k) => {
+      localStorage.removeItem(k);
+    });
 
+    localStorage.clear();
   });
 
   it('should be created', () => {
@@ -31,8 +51,25 @@ describe('StorageService', () => {
   });
 
   it('should getKey', () => {
-    const key = 'loc8r-token';
-    const value = 'loc8r-value';
-    expect(serviceSpy.getItem(key)).toBe(value);
+    expect(serviceSpy.getItem(key)).toBeNull();
+
+    serviceSpy.setItem(key, value);
+    expect(serviceSpy.getItem(key)).toEqual(value);
+  });
+
+  it('should setItem', () => {
+    expect(serviceSpy.getItem(key)).toBeNull();
+
+    serviceSpy.setItem(key, value);
+    expect(serviceSpy.getItem(key)).toEqual(value);
+  });
+
+  it('should removeItem', () => {
+    serviceSpy.setItem(key, value);
+    expect(serviceSpy.getItem(key)).toEqual(value);
+
+    serviceSpy.removeItem(key);
+
+    expect(serviceSpy.getItem(key)).toBeNull();
   });
 });
