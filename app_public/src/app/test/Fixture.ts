a817f3757430b2db9499349ae7211eb59b4a57a3
iproman <itechman@yandex.ru>
2021-03-18 15:34:16 +0500

    Add fixture

---
+++ app_public/src/app/test/Fixture.ts
@@ -0,0 +1,10 @@
+import { User } from '../models/User';
+
+export const userFixture: User = {
+  email: 'test@google.com',
+  name: 'Test Test',
+  password: '123456'
+};
+
+export const tokenNameFixture = 'loc8r-token-test';
+export const tokenValueFixture = 'loc8r-value-test';
