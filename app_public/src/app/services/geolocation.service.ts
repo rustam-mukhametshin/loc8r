d1a35e4a2874e0acea6eb05f0030cfc6a902d9df
iproman <itechman@yandex.ru>
2021-03-25 18:26:29 +0500

    Add geolocation methods

--- app_public/src/app/services/geolocation.service.ts
+++ app_public/src/app/services/geolocation.service.ts
@@ -1,11 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { NAVIGATOR } from '@ng-web-apis/common';
+import { MessageService } from './message.service';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+interface L {
+  lat: number;
+  lng: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeolocationService {
 
-  constructor() {
+  private subject = new BehaviorSubject<L>(null);
+  private location: Observable<L> = this.subject.asObservable();
+
+  constructor(
+    @Inject(NAVIGATOR) private navigator: Navigator,
+    private messageService: MessageService
+  ) {
   }
 
   /**
@@ -21,5 +35,41 @@ export class GeolocationService {
     } else {
       cbNoGeo();
     }
+  }
+
+  /**
+   * Set geo position
+   */
+  setPosition(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.cbSuccess.bind(this), this.cbError.bind(this));
+    } else {
+      this.cbNoGeo();
+    }
+  }
+
+  /**
+   * Success
+   */
+  private cbSuccess(position: GeolocationPosition): void {
+    const obj: L = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+    this.subject.next(obj);
+  }
+
+  /**
+   * Error
+   */
+  private cbError(err: any): void {
+    this.messageService.showErrors(err.message);
+  }
+
+  /**
+   * No geo
+   */
+  private cbNoGeo() {
+    this.messageService.showErrors('Geolocation not supported by this browser');
   }
 }
