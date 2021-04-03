c29157da9ed794fe7317dab053b35dfbdc6a97e0
iproman <itechman@yandex.ru>
2021-04-03 11:18:23 +0500

    Refactor initLocation

--- src/app/views/details-page/details-page.component.ts
+++ src/app/views/details-page/details-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { LocationService } from '../../services/location.service';
-import { switchMap } from 'rxjs/operators';
+import { first, switchMap, tap } from 'rxjs/operators';
 import { Location } from '../../models/Location';
 import { PageInfo } from '../../models/PageInfo';
 
@@ -30,21 +30,25 @@ export class DetailsPageComponent implements OnInit, PageInfo {
   };
 
   ngOnInit(): void {
-    const text = ` is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.\n\nIf you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.`;
+    this.initLocation();
+  }
 
+  private initLocation(): void {
     this.route
       .paramMap
       .pipe(
         switchMap((params: ParamMap) => {
           const id = params.get('locationId');
           return this.dataService.getLocationById(id);
-        })
-      )
-      .subscribe((newLocation: Location) => {
+        }),
+        tap((newLocation: Location) => {
+          const text = ` is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.\n\nIf you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.`;
+
           this.pageContent.header.title = newLocation.name;
           this.pageContent.sidebar = `${newLocation.name} ${text}`;
-          this.location = newLocation;
-        }
-      );
+        }),
+        first()
+      )
+      .subscribe((newLocation: Location) => this.location = newLocation);
   }
 }
